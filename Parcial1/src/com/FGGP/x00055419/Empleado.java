6c66c02af9e402813a6275a8135d4ca59aae0121
Felix palacios <00055419@uca.edu.sv>
2020-04-17 22:28:24 -0600

    Create: new project

--- Parcial1/src/com/FGGP/x00055419/Empleado.java
+++ Parcial1/src/com/FGGP/x00055419/Empleado.java
@@ -1,6 +1,39 @@
 package com.FGGP.x00055419;
-
+import java.util.ArrayList;
 public abstract class Empleado {
-    protected String nombre;
-    protected String puesto;
+    protected String nombre, puesto;
+    protected ArrayList<Documento> documento;
+    protected double salario;
+
+    public Empleado(String nombre, String puesto, double salario) {
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public ArrayList<Documento> getDocumento() {
+        return documento;
+    }
+    public addDocumento(Documento){
+
+    }
+    public removeDocumento(String documento){
+
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 }
