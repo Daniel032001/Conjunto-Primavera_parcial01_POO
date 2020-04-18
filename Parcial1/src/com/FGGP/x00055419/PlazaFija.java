b3186de79e5c0c993c6ffb372f10d04170e5b03e
Daniel032001 <54299423+Daniel032001@users.noreply.github.com>
2020-04-17 22:58:27 -0600

    Modify: CalculadoraImpuestos class modifyied

--- Parcial1/src/com/FGGP/x00055419/PlazaFija.java
+++ Parcial1/src/com/FGGP/x00055419/PlazaFija.java
@@ -2,5 +2,21 @@ package com.FGGP.x00055419;
 
 public class PlazaFija {
     private int extension;
+    public String nombre, puesto;
+    public double salario;
 
+    public PlazaFija(int extension, String nombre, String puesto, double salario) {
+        this.extension = extension;
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.salario = salario;
+    }
+
+    public int getExtension() {
+        return extension;
+    }
+
+    public void setExtension(int extension) {
+        this.extension = extension;
+    }
 }
