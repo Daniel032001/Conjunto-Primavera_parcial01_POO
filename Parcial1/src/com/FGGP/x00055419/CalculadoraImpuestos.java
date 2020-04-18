2e0636ce6210e1ddb3e5f3264d7bd847a17bd797
Felix palacios <00055419@uca.edu.sv>
2020-04-17 23:07:29 -0600

    Create: new project

--- Parcial1/src/com/FGGP/x00055419/CalculadoraImpuestos.java
+++ Parcial1/src/com/FGGP/x00055419/CalculadoraImpuestos.java
@@ -1,13 +1,16 @@
 package com.FGGP.x00055419;
 
-public static class CalculadoraImpuestos {
+public final class CalculadoraImpuestos {
     private double totalRenta, totalISSS, totalAFP;
 
     private CalculadoraImpuestos(){
     }
-    public double calcularPago(Empleado){
+    public double calcularPago(Empleado emp){
+        double pago=0;
+        return pago;
     }
     public String mostrarTotales(){
-
+     String valor="vacio";
+     return valor;
     }
 }
--- Parcial1/src/com/FGGP/x00055419/Main.java
+++ Parcial1/src/com/FGGP/x00055419/Main.java
@@ -1,8 +1,41 @@
 package com.FGGP.x00055419;
 
-public class Main {
+import javax.swing.*;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+	Empresa nuevaEmpresa;
+	int op=0;
+	String nombreempresa="";
+	do{
+        nombreempresa=JOptionPane.showInputDialog(null,"Ingresa el nombre de la empresa : ","Crear nueva empresa",JOptionPane.INFORMATION_MESSAGE);
+        if(nombreempresa!=null) {
+            nuevaEmpresa=new Empresa(nombreempresa);
+        }
+    }
+	while(nombreempresa.equals(""));
+    do {
+    op = menu();
+    switch (op){
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+    }
+   }
+    while(op!=5);
+    }
+    public static int menu(){
+    int valor=Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa una opcion \n1)Agregar empleado\n2)Despedir empleado+"+
+            "\n 3)Ver lista de empleados\n4)Calcular sueldo\n5)Mostrar totales", "Menu principal", JOptionPane.QUESTION_MESSAGE));
+    return valor;
     }
 }
