adff342cfbad678329ff0d0466d218a38a0860dd
Daniel032001 <54299423+Daniel032001@users.noreply.github.com>
2020-04-18 16:00:18 -0600

    add: instanceOf in

--- Parcial1/src/com/FGGP/x00055419/CalculadoraImpuestos.java
+++ Parcial1/src/com/FGGP/x00055419/CalculadoraImpuestos.java
@@ -4,43 +4,63 @@ import javax.swing.*;
 public final class CalculadoraImpuestos {
     int tipoBuscado;
     private double totalRenta, totalISSS, totalAFP;
-    public double isss,renta,afp,pagoEmpleado,restante,x;
+    public double ISSS,Renta,AFP,pagoEmpleado,restante,x;
+
 
     private CalculadoraImpuestos(){
     }
-
     public double calcularPago(Empleado empleado){
+        /*Empresa.planilla.forEach((tipoEmpleado)->{
+        });if(Empresa.planilla.contains(""))*/
+        empleado.getSalario()=salario;
         tipoBuscado=Integer.parseInt(JOptionPane.showInputDialog(null,"1) Servicio Provisional\n2) Plaza Fija\nDigite opcion: ","Selecciona el tipo de Empleado",JOptionPane.QUESTION_MESSAGE));
-        switch (tipoBuscado){
+        if(empleado instanceof PlazaFija){
+            AFP=0.0625*empleado.getSalario();
+            ISSS=0.03*empleado.getSalario();
+            restante=empleado.getSalario()-AFP-ISSS;
+            x=restante;
+            if(empleado.getSalario()>=0.01 && empleado.getSalario()<=472.00){
+                Renta=0;
+            } else if(empleado.getSalario()>=472.01 && empleado.getSalario()<=895.24) {
+                Renta = 0.1 * (x - 472) + 17.67;
+            }  else if(empleado.getSalario()>=895.25 && empleado.getSalario()<=2038.10) {
+                Renta = 0.2 * (x - 472) + 17.67;
+            } else if(empleado.getSalario()>=2038.11) {
+                Renta = 0.3 * (x - 472) + 17.67;
+            }
+            pagoEmpleado=restante-Renta;
+        }else if(empleado instanceof ServicioProfesional){
+            Renta = 0.1 * empleado.getSalario();
+            pagoEmpleado = empleado.getSalario() - Renta;
+            JOptionPane.showInputDialog(null, "Total Pago: " + pagoEmpleado, JOptionPane.QUESTION_MESSAGE);
+        }
+       /* switch (tipoBuscado){
             case 1://SERVICIO PROVICIONAL
-                renta=0.1*empleado.getSalario();
-                pagoEmpleado=empleado.getSalario()*renta;
+                Renta=0.1*empleado.getSalario();
+                pagoEmpleado=empleado.getSalario()-Renta;
                 break;
             case 2://PLAZA FIJA
-                afp=0.0625*empleado.getSalario();
-                isss=0.03*empleado.getSalario();
-                restante=empleado.getSalario()-afp-isss;
+                AFP=0.0625*empleado.getSalario();
+                ISSS=0.03*empleado.getSalario();
+                restante=empleado.getSalario()-AFP-ISSS;
                 x=restante;
-                if(restante>=0.01 && restante<=472.00){
-                    renta=0;
-            } else if(restante>=472.01 && restante<=895.24) {
-                    renta = 0.1 * (x - 472) + 17.67;
-                }  else if(restante>=895.25 && restante<=2038.10) {
-                    renta = 0.2 * (x - 472) + 17.67;
-                } else if(restante>=2038.11) {
-                    renta = 0.3 * (x - 472) + 17.67;
+                if(empleado.getSalario()>=0.01 && empleado.getSalario()<=472.00){
+                    Renta=0;
+            } else if(empleado.getSalario()>=472.01 && empleado.getSalario()<=895.24) {
+                    Renta = 0.1 * (x - 472) + 17.67;
+                }  else if(empleado.getSalario()>=895.25 && empleado.getSalario()<=2038.10) {
+                    Renta = 0.2 * (x - 472) + 17.67;
+                } else if(empleado.getSalario()>=2038.11) {
+                    Renta = 0.3 * (x - 472) + 17.67;
                 }
-                pagoEmpleado=restante-renta;
-                break;
-        }
-        return pagoEmpleado;
+                pagoEmpleado=restante-Renta;
+                break;`
+        }*/
     }
     public String mostrarTotales(){
-        totalISSS+=isss;
-        String mensaje="";
-        totalRenta+=renta;
-        totalAFP+=afp;
-        JOptionPane.showMessageDialog(null,"Total AFP: "+totalAFP+"\nTotal ISSS: "+totalISSS+"\nTotal Renta: "+totalRenta,"Totales",JOptionPane.INFORMATION_MESSAGE);
-        return mensaje;
+        totalISSS+=ISSS;
+        totalRenta+=Renta;
+        totalAFP+=AFP;
+        JOptionPane.showInputDialog(null,"Total AFP: "+totalAFP+"\nTotal ISSS: "+totalISSS+"\nTotal Renta: "+totalRenta,"Totales",JOptionPane.QUESTION_MESSAGE));
     }
 }
