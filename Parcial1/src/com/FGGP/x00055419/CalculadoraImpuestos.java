64b29b48d08a34661e479bb97502b142826a615d
Daniel032001 <54299423+Daniel032001@users.noreply.github.com>
2020-04-17 22:45:57 -0600

    Add: CalculadoraImpuestos created

---
+++ Parcial1/src/com/FGGP/x00055419/CalculadoraImpuestos.java
@@ -0,0 +1,13 @@
+package com.FGGP.x00055419;
+
+public static class CalculadoraImpuestos {
+    private double totalRenta, totalISSS, totalAFP;
+
+    private CalculadoraImpuestos(){
+    }
+    public double calcularPago(Empleado){
+    }
+    public String mostrarTotales(){
+
+    }
+}
---
+++ Parcial1/src/com/FGGP/x00055419/PlazaFija.java
@@ -0,0 +1,6 @@
+package com.FGGP.x00055419;
+
+public class PlazaFija {
+    private int extension;
+
+}
