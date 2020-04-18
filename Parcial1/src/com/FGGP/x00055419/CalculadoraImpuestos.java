0527e1f09aa989734bef599fa8ada996c31cfbde
Daniel032001 <54299423+Daniel032001@users.noreply.github.com>
2020-04-18 01:59:13 -0600

    Add: total variables

--- Parcial1/src/com/FGGP/x00055419/CalculadoraImpuestos.java
+++ Parcial1/src/com/FGGP/x00055419/CalculadoraImpuestos.java
@@ -1,54 +1,43 @@
 package com.FGGP.x00055419;
 import javax.swing.*;
 
-<<<<<<< HEAD
-public static class CalculadoraImpuestos {
-    int tipoBuscado;
-=======
 public final class CalculadoraImpuestos {
->>>>>>> bcecf5aae190c88259fa1f58e924a947aae08e90
+    int tipoBuscado;
     private double totalRenta, totalISSS, totalAFP;
-    public double pagoEmpleado,restante,x;
+    public double isss,renta,afp,pagoEmpleado,restante,x;
 
     private CalculadoraImpuestos(){
     }
-<<<<<<< HEAD
 
     public double calcularPago(Empleado empleado){
         tipoBuscado=Integer.parseInt(JOptionPane.showInputDialog(null,"1) Servicio Provisional\n2) Plaza Fija\nDigite opcion: ","Selecciona el tipo de Empleado",JOptionPane.QUESTION_MESSAGE));
         switch (tipoBuscado){
             case 1://SERVICIO PROVICIONAL
-                totalRenta=0.1*salario;
-                pagoEmpleado=salario*totalRenta;
+                renta=0.1*empleado.getSalario();
+                pagoEmpleado=empleado.getSalario()*renta;
                 break;
             case 2://PLAZA FIJA
-                totalAFP=0.0625*salario;
-                totalISSS=0.03*salario;
-                restante=salario-totalAFP-totalISSS;
+                afp=0.0625*empleado.getSalario();
+                isss=0.03*empleado.getSalario();
+                restante=empleado.getSalario()-afp-isss;
                 x=restante;
                 if(restante>=0.01 && restante<=472.00){
-                    totalRenta=0;
+                    renta=0;
             } else if(restante>=472.01 && restante<=895.24) {
-                    totalRenta = 0.1 * (x - 472) + 17.67;
+                    renta = 0.1 * (x - 472) + 17.67;
                 }  else if(restante>=895.25 && restante<=2038.10) {
-                    totalRenta = 0.2 * (x - 472) + 17.67;
+                    renta = 0.2 * (x - 472) + 17.67;
                 } else if(restante>=2038.11) {
-                    totalRenta = 0.3 * (x - 472) + 17.67;
+                    renta = 0.3 * (x - 472) + 17.67;
                 }
-                pagoEmpleado=restante-totalRenta;
+                pagoEmpleado=restante-renta;
                 break;
         }
     }
     public String mostrarTotales(){
+        totalISSS+=isss;
+        totalRenta+=renta;
+        totalAFP+=afp;
         JOptionPane.showInputDialog(null,"Total AFP: "+totalAFP+"\nTotal ISSS: "+totalISSS+"\nTotal Renta: "+totalRenta,"Totales",JOptionPane.QUESTION_MESSAGE));
-=======
-    public double calcularPago(Empleado emp){
-        double pago=0;
-        return pago;
-    }
-    public String mostrarTotales(){
-     String valor="vacio";
-     return valor;
->>>>>>> bcecf5aae190c88259fa1f58e924a947aae08e90
     }
 }
--- Parcial1/src/com/FGGP/x00055419/PlazaFija.java
+++ Parcial1/src/com/FGGP/x00055419/PlazaFija.java
@@ -1,19 +1,16 @@
 package com.FGGP.x00055419;
 
 public class PlazaFija extends Empleado {
-<<<<<<< HEAD
     int extension;
     public PlazaFija(String nombre, String puesto, double salario,int extension ) {
-=======
-    private int extension;
-    public PlazaFija(String nombre, String puesto, double salario,int extension) {
->>>>>>> bcecf5aae190c88259fa1f58e924a947aae08e90
         super(nombre, puesto, salario);
         this.extension=extension;
     }
+
     public int getExtension() {
         return extension;
     }
+
     public void setExtension(int extension) {
         this.extension = extension;
     }
